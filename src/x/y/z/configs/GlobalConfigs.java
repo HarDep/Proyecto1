50c704bc47b44dedd2f59411e84fbd626326c81a
HarDep <haroldralvaradol@gmail.com>
2023-06-11 20:23:44 -0500

    Change config access

--- src/x/y/z/configs/GlobalConfigs.java
+++ src/x/y/z/configs/GlobalConfigs.java
@@ -5,6 +5,6 @@ import java.awt.*;
 public class GlobalConfigs {
     public static final Dimension TOTAL_DIMENSION = new Dimension(1000,700);
     protected static final String PATH_CONFIGS_PROPERTIES = "resources/config.properties";
-    protected static final String PATH_LANGUAGE_ENGLISH = "resources/en.properties";
-    protected static final String PATH_LANGUAGE_SPANISH = "resources/es.properties";
+    public static final String PATH_LANGUAGE_ENGLISH = "resources/en.properties";
+    public static final String PATH_LANGUAGE_SPANISH = "resources/es.properties";
 }
