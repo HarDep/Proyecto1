7f7924c9c176a2a7f27a2a3e5f3e2a7771636d07
Sebastian <sebastiandd87@gmail.com>
2023-06-15 14:42:28 -0500

    create method updateUsers in class ManagersUsers and add lib Gson

--- src/x/y/z/models/ManagerUsers.java
+++ src/x/y/z/models/ManagerUsers.java
@@ -2,6 +2,10 @@ package x.y.z.models;
 
 import x.y.z.pojos.User;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import java.io.*;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,5 +14,16 @@ public class ManagerUsers {
 
     public ManagerUsers() {
         users = new ArrayList<>();
+    }
+
+    private void updateUsers(){
+        try {
+            String json = new Gson().toJson(users);
+            BufferedWriter writer = new BufferedWriter(new FileWriter("data/users.json"));
+            writer.write(json);
+            writer.close();
+        }catch (IOException e){
+           System.out.println(e);
+        }
     }
 }
