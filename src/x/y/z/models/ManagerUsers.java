da9c0d17706c7c7f129f5e8717187a9ea58a68ff
naju <juanpablomartinezg50@gmail.com>
2023-06-20 17:06:54 -0500

    metodo para leer los archivos json y ya no se si era asi me guie por un video de youtube ajajaja

--- src/x/y/z/models/ManagerUsers.java
+++ src/x/y/z/models/ManagerUsers.java
@@ -2,6 +2,13 @@ package x.y.z.models;
 
 import x.y.z.pojos.User;
 
+import java.io.*;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,4 +18,28 @@ public class ManagerUsers {
     public ManagerUsers() {
         users = new ArrayList<>();
     }
+
+    private void readUsers(){
+
+        JSONParser jsonParser = new JSONParser();
+        try(FileReader = new FileReader("Users.json")){
+            Object obj = jsonParser.parse(reader);
+            JSONArray Users = (JSONArray) obj;
+
+        for (Object User: Users){
+            showUsers((JSONObject)user);
+        }
+
+        }catch(FileNotFoundException e){
+            e.printStackTrace();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+        catch(ParseException e){
+            e.printStackTrace();
+        }
+    }
+
+
 }
